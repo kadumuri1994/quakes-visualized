d8a0a35c4469e1e4755a83d13bb0d0c3d6bf51ce
Kadumuri, Vamshi <vamshi.kadumuri@spglobal.com>
2021-03-26 13:31:30 +0530

    lint fixes

--- app/app/routing.py
+++ app/app/routing.py
@@ -1,5 +1,5 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter
+
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
